dd7b45b4a621020250f4c31448537b11b83e14ee
Sajith Sasidharan <sajith@hcoop.net>
2021-01-16 13:54:49 -0500

    Use utf-8 encoding when opening README
    This is a problem in when running setup on Windows and Python 3.x.

--- setup.py
+++ setup.py
@@ -358,7 +358,7 @@ if version:
 
 setup(name="tahoe-lafs", # also set in __init__.py
       description='secure, decentralized, fault-tolerant file store',
-      long_description=open('README.rst', 'r').read(),
+      long_description=open('README.rst', 'r', encoding='utf-8').read(),
       author='the Tahoe-LAFS project',
       author_email='tahoe-dev@tahoe-lafs.org',
       url='https://tahoe-lafs.org/',
